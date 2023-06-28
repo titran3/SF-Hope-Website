a59fc0848ba5e76fd85da7caa64709be25a132d9
titran3 <121971162+titran3@users.noreply.github.com>
2023-06-27 23:01:55 -0700

    added stuff

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -6,4 +6,123 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
-})
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].classList.add("hidden");
+  }
+});
+
+function toggleDropdown(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+  var link = document.getElementById(dropdownId + "Link");
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var links = document.getElementsByClassName("dropbtn");
+
+  for (var i = 0; i < dropdowns.length; i++) {
+    var currentDropdown = dropdowns[i];
+    var currentLink = links[i];
+
+    if (currentDropdown === dropdown) {
+      currentDropdown.classList.toggle("hidden");
+      currentDropdown.classList.toggle("fade-in");
+      currentLink.classList.toggle("active");
+    } else {
+      currentDropdown.classList.add("hidden");
+      currentDropdown.classList.remove("fade-in");
+      currentLink.classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("click", function(event) {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var links = document.getElementsByClassName("dropbtn");
+
+  var isDropdownClick = false;
+
+  for (var i = 0; i < dropdowns.length; i++) {
+    if (event.target === dropdowns[i] || event.target === links[i]) {
+      isDropdownClick = true;
+      break;
+    }
+  }
+
+  if (!isDropdownClick) {
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      var openLink = links[i];
+      if (openDropdown.classList.contains('fade-in')) {
+        openDropdown.classList.remove('fade-in');
+        openDropdown.classList.add('hidden');
+        openLink.classList.remove('active');
+      }
+    }
+  }
+});
+
+var dropdownLinks = document.querySelectorAll(".dropdown-content a");
+for (var i = 0; i < dropdownLinks.length; i++) {
+  dropdownLinks[i].addEventListener("mouseenter", function() {
+    this.style.color = "#fa65b1";
+  });
+
+  dropdownLinks[i].addEventListener("mouseleave", function() {
+    if (!this.classList.contains("active")) {
+      this.style.color = "";
+    }
+  });
+}
+
+var navLinks = document.querySelectorAll(".nav li:not(.dropdown) a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("mouseenter", function() {
+    for (var j = 0; j < navLinks.length; j++) {
+      if (!navLinks[j].classList.contains("active")) {
+        navLinks[j].style.color = "";
+      }
+    }
+    if (!this.classList.contains("active") && !this.classList.contains("initiative")) {
+      this.style.color = "#fa65b1";
+    }
+  });
+
+  navLinks[i].addEventListener("mouseleave", function() {
+    if (!this.classList.contains("active") && !this.classList.contains("initiative")) {
+      this.style.color = "";
+    }
+  });
+}
+
+var dropdownLinks = document.querySelectorAll(".dropdown-content a");
+for (var i = 0; i < dropdownLinks.length; i++) {
+  dropdownLinks[i].addEventListener("mouseenter", function() {
+    for (var j = 0; j < dropdownLinks.length; j++) {
+      if (!dropdownLinks[j].classList.contains("active")) {
+        dropdownLinks[j].style.color = "";
+      }
+    }
+    if (!this.classList.contains("active")) {
+      this.style.color = "#fa65b1";
+    }
+  });
+
+  dropdownLinks[i].addEventListener("mouseleave", function() {
+    if (!this.classList.contains("active")) {
+      this.style.color = "";
+    }
+  });
+}
+
+// Apply initial active state
+var activeLinks = document.querySelectorAll(".nav li a.active");
+for (var i = 0; i < activeLinks.length; i++) {
+  activeLinks[i].style.color = "#fa65b1";
+}
+
+// Set the color of "Initiative" link to pink
+var initiativeLink = document.getElementById("initiativeLink");
+initiativeLink.style.color = "#fa65b1";
